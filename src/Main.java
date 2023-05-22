2807b497cfe6c049d345ab0d9cadc83f9e2d373b
kolenpat <patrik.kolenovsky@tietoevry.com>
2023-05-22 09:51:10 +0200

    Chages to intro line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Hello my name is Martin and ...");
+
         System.out.println("Goodbye cruel world!");
     }
 }
